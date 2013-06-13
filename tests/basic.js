a0c85a428105fbe8dae4662b6c967dcd48ef7958
Aaron O'Mullan <aaron.omullan@friendco.de>
2013-06-13 17:04:26 +0200

    Fix: test sample

--- tests/basic.js
+++ tests/basic.js
@@ -1,8 +1,8 @@
 var http = require('http');
-var loadfire = require('../lib/index');
+var loadfire = require('../');
 
 
-var EDITOR_PORTS = [7001, 7002, 7003, 7004];
+var EDITOR_PORTS = [7001, 7002, 7003,7004];
 
 var EDITOR_SERVERS = EDITOR_PORTS.map(function (x) {
     return {
@@ -11,27 +11,57 @@ var EDITOR_SERVERS = EDITOR_PORTS.map(function (x) {
     };
 });
 
-var CONFIG = {
-    'editor.io': {
-        backends: EDITOR_SERVERS,
-        pattern: 
-    }
-};
-
+// Start all our different http servers
 function startEditorServers(ports) {
     ports.forEach(function (port) {
-        http.createServer(function(req, res) {
-            res.send(port);
-            res.close();
+        // Setup HTTP Server
+        var httpServer = http.createServer(function(req, res) {
+            // Output the port number the server is running on
+            res.writeHead(200, {'Content-Type': 'text/plain'});
+            res.end(port.toString());
         });
+
+        // Listen on assigned port
+        httpServer.listen(port);
     });
 }
 
+// Config for our loadfire server
+var CONFIG = {
+    'resources': [
+        {
+            // resource is some value identify this resource
+            // by default it should be the hostname to match
+            resource: 'localhost:8000',
+
+            // List of backends to hit
+            backends: EDITOR_SERVERS,
+
+            // Load balancing pattern
+            // As of now a few are builtin
+            // random, roundrobin, sticky
+            pattern: 'roundrobin'
+        }
+    ],
+
+    // Server to start loadfire on
+    port: 8000
+};
+
 function main() {
+    // Start our http servers
     startEditorServers(EDITOR_PORTS);
-    var group = loadfire.createLoadbalancer(BACKENDS, {
-        'edtior.io': loadfire.LoadBalancer
-    });
+
+    // Setup our load balancer with the above config
+    var loadServer = loadfire.createServer(CONFIG);
+
+    // Now start our load balancer
+    loadServer.run();
+
+    // Check out localhost:8000
+    // Refresh a few times and you'll see different port numbers appear
+    // depending on which http server the requests are proxied to
+    // since we are using the roundrobin pattern it will cycle through them
 }
 
 // Run main
