3622aef23d371f83844d00fb1fffe82f6a570d46
Aaron O'Mullan <aaron.omullan@friendco.de>
2013-09-30 01:12:28 -0700

    Expose config module

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ var selectors = require('./lib/selectors/');
 var balancers = require('./lib/balancers/');
 
 var groupBalancer = require('./lib/groupbalancer');
+var config = require('./lib/config');
 
 
 function createServer(config) {
@@ -23,3 +24,4 @@ exports.selectors = selectors;
 exports.balancers = balancers;
 
 exports.groupBalancer = groupBalancer;
+exports.config = config;
